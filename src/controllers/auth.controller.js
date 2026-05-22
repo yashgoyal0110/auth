c4f66c89550a0c642db616d3d754aaff68269881
yashgoyal0110 <yg364550@gmail.com>
2026-05-22 21:39:59 +0530

    auth-code

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res) => {
     return res.status(400).json({ message: "All fields are required" });
   }
 
-  const existingUser = await User.findOne({ email });
+  const existingUser = await User.findOne({ email });  
   if (existingUser) {
     return res.status(409).json({ message: "Email already registered" });
   }
--- src/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (data) => {
-  return jwt.sign({ ...data }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-};
-export default generateToken;
